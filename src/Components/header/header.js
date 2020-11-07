1d93a3cd72802886f9a01990f361791956e37067
samuelige <samuelige.iyanu@gmail.com>
2020-11-07 23:18:05 +0100

    added mobile menu icon

--- src/Components/header/header.js
+++ src/Components/header/header.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import DehazeIcon from '@material-ui/icons/Dehaze';
 import './header.scss'
 
 const Header = () => {
     return (
         <div className="header">
+            <button className='menu-btn'><DehazeIcon/></button>
             <h4 className="logo"> room </h4>
             <div className="navigationBar">
                 <h5>home</h5>
